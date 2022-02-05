e4f9cc75d5dfab32f085718d4a82dc8f435321bd
igorGavr <ragamuffinrogi@gmail.com>
2022-02-05 18:14:27 +0200

    lesson3 additional_work

--- lesson3/additional/main.js
+++ lesson3/additional/main.js
@@ -2,6 +2,8 @@
 Використовуючи данні з масиву, за допомоги document.write та циклу
 побудувати структуру по шаблону template1.1
 */
+document.write(`<div class="main_wrap">`)
+
 
 let listOfItems = ['html', 'css', 'javascript', 'mysql',
                    'mongodb', 'react', 'angular', 'node.js'];
@@ -40,7 +42,7 @@ let simpsons = [
         name: 'Marge',
         surname: 'Simpson',
         age: 38,
-        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер.  Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города.',
         photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
     },
     {
@@ -54,28 +56,27 @@ let simpsons = [
         name: 'Maggie',
         surname: 'Simpson',
         age: 1,
-        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
         photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
     },
 ];
-
-// document.write(`<div>`)
-
+document.write(`<div class="main">`)
 for (let i = 0; i < simpsons.length; i++) {
     // let simpson = simpsons[i];
-    document.write(`<div class="target"><h2>
-                                            ${simpsons[i].name}
-                                            ${simpsons[i].surname} . Age is - 
-                                            ${simpsons[i].age} 
-                                         </h2>
-                                         ${simpsons[i].info}
-                                         <div class="foto">
-                                            <img src="${simpsons[i].photo}" alt="foto here">
-                                         </div>
+    document.write(`<div class="target">
+                            <h2>
+                                ${simpsons[i].name}
+                                ${simpsons[i].surname} . Age is - 
+                                ${simpsons[i].age} 
+                            </h2>
+                            ${simpsons[i].info}
+                            <div class="foto">
+                                <img src="${simpsons[i].photo}" alt="foto here">
+                            </div>
+                        
                     </div>`)
 }
-// document.write(`</div>`)
-
+document.write(`</div>`)
 
 /*
     Використовуючи данні з масиву, за допомоги document.write та циклу
@@ -105,7 +106,6 @@ let products = [
         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
     },
 ];
-
 document.write(`<div>`)
 for (let product of products) {
     document.write(`<div class="product-card">
@@ -114,6 +114,346 @@ for (let product of products) {
                             . Price - ${product.price}
                         </h3>
                         <img class="product-image" src="${product.image}" alt="" >
+                    </div>`)
+}
+document.write(`</div>`)
+
+/*За допомогою циклу вивести всю інформацію про пости використовуючи шаблон Template 1.1.
+* ЗА допомоги стилів побудувати структуру "сітки" по 5 об'єктів в лінію
+* */
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+document.write(`<div class="main2">`)
+for (const coursesArrayElement of coursesArray) {
+    document.write(`<div class="wrap">
+                        <div class="post-card">
+                            <h3>Курс - ${coursesArrayElement.title} , 
+                            </h3>
+                        буде тривати - 
+                                ${coursesArrayElement.monthDuration} , загальним обсягом - 
+                                ${coursesArrayElement.hourDuration} годин! Алілуя!!!
+                        </div>
+                        <p>В курс входять такі модулі: 
+                            ${coursesArrayElement.modules}
+                        </p>
+
+                   </div>`)
+}
+document.write(`</div>`)
+document.write(`</div>`)
+
+let usersList = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+                lat: '-37.3159',
+                lng: '81.1496'
+            }
+        },
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+        company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets'
+        }
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        address: {
+            street: 'Victor Plains',
+            suite: 'Suite 879',
+            city: 'Wisokyburgh',
+            zipcode: '90566-7771',
+            geo: {
+                lat: '-43.9509',
+                lng: '-34.4618'
+            }
+        },
+        phone: '010-692-6593 x09125',
+        website: 'anastasia.net',
+        company: {
+            name: 'Deckow-Crist',
+            catchPhrase: 'Proactive didactic contingency',
+            bs: 'synergize scalable supply-chains'
+        }
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        address: {
+            street: 'Douglas Extension',
+            suite: 'Suite 847',
+            city: 'McKenziehaven',
+            zipcode: '59590-4157',
+            geo: {
+                lat: '-68.6102',
+                lng: '-47.0653'
+            }
+        },
+        phone: '1-463-123-4447',
+        website: 'ramiro.info',
+        company: {
+            name: 'Romaguera-Jacobson',
+            catchPhrase: 'Face to face bifurcated interface',
+            bs: 'e-enable strategic applications'
+        }
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        address: {
+            street: 'Hoeger Mall',
+            suite: 'Apt. 692',
+            city: 'South Elvis',
+            zipcode: '53919-4257',
+            geo: {
+                lat: '29.4572',
+                lng: '-164.2990'
+            }
+        },
+        phone: '493-170-9623 x156',
+        website: 'kale.biz',
+        company: {
+            name: 'Robel-Corkery',
+            catchPhrase: 'Multi-tiered zero tolerance productivity',
+            bs: 'transition cutting-edge web services'
+        }
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        address: {
+            street: 'Skiles Walks',
+            suite: 'Suite 351',
+            city: 'Roscoeview',
+            zipcode: '33263',
+            geo: {
+                lat: '-31.8129',
+                lng: '62.5342'
+            }
+        },
+        phone: '(254)954-1289',
+        website: 'demarco.info',
+        company: {
+            name: 'Keebler LLC',
+            catchPhrase: 'User-centric fault-tolerant solution',
+            bs: 'revolutionize end-to-end systems'
+        }
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        address: {
+            street: 'Norberto Crossing',
+            suite: 'Apt. 950',
+            city: 'South Christy',
+            zipcode: '23505-1337',
+            geo: {
+                lat: '-71.4197',
+                lng: '71.7478'
+            }
+        },
+        phone: '1-477-935-8478 x6430',
+        website: 'ola.org',
+        company: {
+            name: 'Considine-Lockman',
+            catchPhrase: 'Synchronised bottom-line interface',
+            bs: 'e-enable innovative applications'
+        }
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        address: {
+            street: 'Rex Trail',
+            suite: 'Suite 280',
+            city: 'Howemouth',
+            zipcode: '58804-1099',
+            geo: {
+                lat: '24.8918',
+                lng: '21.8984'
+            }
+        },
+        phone: '210.067.6132',
+        website: 'elvis.io',
+        company: {
+            name: 'Johns Group',
+            catchPhrase: 'Configurable multimedia task-force',
+            bs: 'generate enterprise e-tailers'
+        }
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        address: {
+            street: 'Ellsworth Summit',
+            suite: 'Suite 729',
+            city: 'Aliyaview',
+            zipcode: '45169',
+            geo: {
+                lat: '-14.3990',
+                lng: '-120.7677'
+            }
+        },
+        phone: '586.493.6943 x140',
+        website: 'jacynthe.com',
+        company: {
+            name: 'Abernathy Group',
+            catchPhrase: 'Implemented secondary concept',
+            bs: 'e-enable extensible e-tailers'
+        }
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        address: {
+            street: 'Dayna Park',
+            suite: 'Suite 449',
+            city: 'Bartholomebury',
+            zipcode: '76495-3109',
+            geo: {
+                lat: '24.6463',
+                lng: '-168.8889'
+            }
+        },
+        phone: '(775)976-6794 x41206',
+        website: 'conrad.com',
+        company: {
+            name: 'Yost and Sons',
+            catchPhrase: 'Switchable contextually-based project',
+            bs: 'aggregate real-time technologies'
+        }
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        address: {
+            street: 'Kattie Turnpike',
+            suite: 'Suite 198',
+            city: 'Lebsackbury',
+            zipcode: '31428-2261',
+            geo: {
+                lat: '-38.2386',
+                lng: '57.2232'
+            }
+        },
+        phone: '024-648-3804',
+        website: 'ambrose.net',
+        company: {
+            name: 'Hoeger LLC',
+            catchPhrase: 'Centralized empowering task-force',
+            bs: 'target end-to-end models'
+        }
+    }
+];
+document.write(`<div class="wrap_user_list" >`)
+for (const usersListElement of usersList) {
+    document.write(`<div class="users-box">
+                        <div class="user-block">
+                            <h2>${usersListElement.id} - ${usersListElement.name} - ${usersListElement.username} </h2>
+                            <h3>${usersListElement.email} - ${usersListElement.phone}</h3>
+                            <div class="address-block">
+                                <p>City - ${usersListElement.address.city}</p>
+                                <p> Street - ${usersListElement.address.street}</p>
+                                <p> Suite - ${usersListElement.address.suite}</p>
+                                <p>Zip code - ${usersListElement.address.zipcode}</p>
+                            </div>
+                        </div>
                     </div>`)
 }
 document.write(`</div>`)
