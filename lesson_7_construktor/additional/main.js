da833ec5d724a625161001c982a653d7d92156c4
igorGavr <ragamuffinrogi@gmail.com>
2022-02-18 17:02:43 +0200

    lesson7 additional

--- lesson_7_construktor/additional/main.js
+++ lesson_7_construktor/additional/main.js
@@ -65,17 +65,17 @@ let user1Geo = new Geo(23, 233)
 let user1Address = new Address('Bakery', 2323, 'New-York',
     121212, user1Geo)
 let user1Company = new Company('Pivo International', 'Drink or die', 'fogi')
-let user1 = new User(0, 'Gans', 'Vasya', 'adfs@df.com',
+let userA = new User(0, 'Gans', 'Vasya', 'adfs@df.com',
    user1Address, 3802323232, 'www.alko-tur.com',user1Company)
 
-console.log(user1)
+console.log(userA)
 
-let user2 = new User(1, 'Oleh', 'Mitla', 'knock@heaven.door',
+let userB = new User(1, 'Oleh', 'Mitla', 'knock@heaven.door',
     new Address('doood', 23, 'Denver', 121212,
         new Geo(232, 323)),380232323, 'www.oops@rock.com',
     new Company('music for morons', 'Your ears are my waste basket', 'jimi'))
 
-console.log(user2)
+console.log(userB)
 // -  Створити функцію конструктор / клас  який описує об'єкт тегу
 // Поля :
 //     -назва тегу ()
@@ -139,62 +139,158 @@ console.log(a, div, h1, span, input, form, option, select)
 
 console.log('===========================')
 
-let number = Math.floor(Math.random()*(4-1)+1)
-console.log(number)
+let quantityOfCards = 0
 
+function userCard(number) {  // Створити функцію «userCard» яка приймає число(будь-яке число)
 
-
-
-class UserCard {
-    constructor(balance, transactionLimit, historyLogs, key) {
-        this.balance = 100;
-        this.transactionLimit = 100;
-        this.historyLogs = historyLogs;
-        this.key = number;
-    }
-    putCredits(quantity) {
-        this.balance += quantity
-        console.log(this.balance)
-        console.log(quantity)
-    }
-    takeCredits(quantity) {
-        if (this.balance > quantity) {
-            this.balance -= quantity
-        } else {
-            console.error('you dont have money')
+    let objCard = {
+        balance: 100,
+        transactionLimit: 100,
+        historyLogs: [],
+        get key() {
+            if (number === undefined) {
+                number = ++quantityOfCards
+            }
+            if (number >= 1 && number <= 3) {
+                return number
+            } else {
+                console.error(`Параметр key має бути числом в діапазоні[1; 3]`)
+                return -1
+            }
         }
     }
-    setTransactionLimit(quantity) {
-        this.transactionLimit = quantity
-    }
-    transferCredits(quantityOfCredits, user2) {
-        let tax = quantityOfCredits*0.005
-        if (tax+quantityOfCredits>this.balance) {
-            console.error('you dont have enough money')
-        } else {
-            user2.balance+=quantityOfCredits
-            this.balance-=(quantityOfCredits+tax)
-            this.historyLogs.operationType = 'Withdrawal of credits'
-            this.historyLogs.credits = quantityOfCredits
-            this.operationTime = '03/32/32'
+    return {
+        getCardOptions: function () { // Дана функція повертає об’єкт котрий містить інформацію про карту
+            return Object.create(    // создаём новый объект, чей прототип
+                {},              // является новым пустым объектом
+                {         // и добавляем свойства из нашей карты
+                    balance: {value: objCard.balance,},
+                    transactionLimit: {value: objCard.transactionLimit,},
+                    historyLogs: {value: objCard.historyLogs},
+                    key: {value: objCard.key}
+                })
+        },
+        putCredits: function (balance) {  //функція отримує певну кількість грошей і заповнює баланс карти
+            objCard.balance += balance
+
+            objCard.historyLogs.push({
+                operationType: 'Received credits',
+                credits: balance,
+                operationTime: timeOfOperation()
+            })
+        },
+        takeCredits: function (quantity) {   //функція  віднімає  кошти з баланса карти
+            if (quantity <= objCard.transactionLimit && quantity <= objCard.balance) {
+                objCard.balance -= quantity
+
+                objCard.historyLogs.push({
+                    operationType: 'Withdrawal of credits',
+                    credits: quantity,
+                    operationTime: timeOfOperation()
+                })
+            } else {
+                console.error('Сума, яку Ви бажаєте взяти, перевищує ліміт транзакцій та залишок коштів на карті')
+            }
+        },
+        setTransactionLimit: function (limit) {  // функція приймає як аргумент число і встановлює його
+            objCard.transactionLimit = limit    //як ліміт транзакцій на картці
+
+            objCard.historyLogs.push({
+                operationType: 'Transaction limit change',
+                credits: limit,
+                operationTime: timeOfOperation()
+            })
+        },
+        transferCredits: function (credit, recipientCard) {  //я функція отримує два аргументи - кількість
+
+            let tax = credit * 0.005      //кредитів, які потрібно передати,та карту одержувача
+
+            if (credit + tax > objCard.transactionLimit) {
+                console.error(`Ви не можете перерахувати кошти через недостатній ліміт ${objCard.transactionLimit}`)
+            } else if (credit + tax > objCard.balance) {
+                console.error(`У вас недостатньо коштів (${objCard.balance} `)
+            } else {
+                this.takeCredits(credit + tax)  // знімаємо кошти з карти відправника
+                recipientCard.putCredits(credit)      // передаємо кошти на карту отримувача
+            }
         }
     }
 }
 
 class UserAccount {
-    name
-    cards
+    constructor(name) {
+        this.name = name
+        this.cards = []  // Масив не має бути довшим ніж 3
 
-    constructor(name, cards) {
-        this.name = name;
-        this.cards = cards;
+        quantityOfCards = 0
     }
-    addCard() {
 
+    addCard() {
+        if (this.cards.length < 3) {
+            let card = userCard()   // створюємо нову карту
+            this.cards.push(card)  // додаємо карту до масиву карт
+        } else {
+            console.error(`Користувач повинен мати <= 3 карти!`)
+        }
+    }
+
+    getCardByKey(key) {
+        if (key > 3) {
+            console.error('Число  має бути в діапазоні від 1 до 3')
+            return {}
+        }
+        if (this.cards.find(item => item.getCardOptions().key === key)) {   // шукаємо карту в масиві карт за ключем
+            return this.cards.find(item => item.getCardOptions().key === key)
+        } else {
+            console.error('Карти з таким ідентифікатором не існує!')
+            return {}
+        }
     }
 }
 
-function userCard(number) {
-    return new UserCard(23, 23, null, 1)
+let user = new UserAccount('Bob')
+let user2 = new UserAccount('Gans')
+console.log(user)
+console.log(user2)
+user.addCard()     // створюємо карту1 для user
+user.addCard()     // створюємо карту2 для user
+
+user2.addCard()     // створюємо карту1 для user2
+
+console.log(user.cards)   // масив карт  користувача user
+console.log(user2.cards)  // масив карт  користувача user2
+
+let card1User = user.getCardByKey(1)  //
+console.log(card1User.getCardOptions())  //{balance: 100, transactionLimit: 100, historyLogs: Array(0), key: 1}
+let card2User = user.getCardByKey(2)
+console.log(card2User.getCardOptions())  // {balance: 100, transactionLimit: 100, historyLogs: Array(0), key: 2}
+
+let card1user2 = user2.getCardByKey(3)
+console.log(card1user2.getCardOptions())  // {balance: 100, transactionLimit: 100, historyLogs: Array(0), key: 3}
+
+card1User.putCredits(155)
+
+console.log(card1User.getCardOptions()) // {balance: 255, transactionLimit: 100, historyLogs: Array(1), key: 1}
+                                       // баланс зріс на 155
+
+card1User.setTransactionLimit(300)
+console.log(card1User.getCardOptions())  // {balance: 255, transactionLimit: 300, historyLogs: Array(2), key: 1}
+                                        // кредитний ліміт зріс до 300
+
+card1User.transferCredits(230, card1user2)  // перерахували 230 на карту card1user2
+console.log(card1user2.getCardOptions())    //{balance: 330, transactionLimit: 100, historyLogs: Array(1), key: 3}
+                                           // баланс зріс з 100  до 330
+console.log(card1User.getCardOptions())   // {balance: 23.849999999999994, transactionLimit: 300, historyLogs: Array(3), key: 1}
+
+card1user2.takeCredits(100)               // зняли з карти card1user2 100
+console.log(card1user2.getCardOptions())  // {balance: 230, transactionLimit: 100, historyLogs: Array(2), key: 3}
+
+function timeOfOperation() {
+    let date = new Date().getDate()
+    let month = new Date().getMonth() + 1
+    let hour = new Date().getHours()
+    let minute = new Date().getMinutes()
+    let second = new Date().getSeconds()
+    return `${date}/${month}/${new Date().getFullYear()}, ${hour}:${minute}:${second}`
 }
 
