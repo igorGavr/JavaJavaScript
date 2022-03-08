97046fd4824d913d5d390a5b4600fbb284b049ad
igorGavr <ragamuffinrogi@gmail.com>
2022-03-08 17:51:26 +0200

    lesson 11

---
+++ lesson_11_/class/favorites.js
@@ -0,0 +1,13 @@
+const favoritesKey = 'favorites';
+const container = document.getElementById('container');
+const users = JSON.parse(localStorage.getItem(favoritesKey));
+
+users.forEach(user => {
+    const userDiv = document.createElement('div');
+    userDiv.className = 'user';
+    const content = document.createElement('div');
+    content.className = 'content'
+    content.innerText = `Name: ${user.name}\nAge: ${user.age}\nStatus: ${user.status}`;
+    userDiv.appendChild(content);
+    container.appendChild(userDiv);
+})
